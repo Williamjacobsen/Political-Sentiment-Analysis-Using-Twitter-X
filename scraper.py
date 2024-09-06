1f7fd011797d5f4f7dca5c13f92b57f9daa1da60
William Jacobsen <villi05.v.j@gmail.com>
2024-09-06 16:22:35 +0200

    setup 2nd driver and chatgpt login start

--- scraper.py
+++ scraper.py
@@ -33,12 +33,15 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 
 chromedriver_autoinstaller.install()
 
-driver = webdriver.Chrome(options=chrome_options)
-wait = WebDriverWait(driver, 20)
+driver1 = webdriver.Chrome(options=chrome_options)
+wait1 = WebDriverWait(driver1, 20)
+
+driver2 = webdriver.Chrome(options=chrome_options)
+wait2 = WebDriverWait(driver2, 20)
 
 os.system("cls")
 
-def clickElement(xpath):
+def clickElement(driver, wait, xpath):
     try:
         showmore_link = wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
         showmore_link.click()
@@ -47,7 +50,7 @@ def clickElement(xpath):
         print("Trying to click on the button again")
         driver.execute_script("arguments[0].click()", showmore_link)
 
-def locateElement(xpath):
+def locateElement(wait, xpath):
     try:
         element = wait.until(EC.presence_of_all_elements_located((By.XPATH, xpath)))
         element = element[0].get_attribute('innerHTML')
@@ -58,7 +61,7 @@ def locateElement(xpath):
         print("\nCould not locate element\n")
         return ""
 
-def send_keysElement(xpath, keys):
+def send_keysElement(wait, xpath, keys):
     try:
         showmore_link = wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
         showmore_link.send_keys(keys)
@@ -66,39 +69,76 @@ def send_keysElement(xpath, keys):
     except Exception:
         print("Couldn't send keys")
 
-def run():
-    driver.get('https://x.com/home')
+def signInTwitter():
+    driver1.get('https://x.com/home')
 
-    
     # sign in
-    clickElement('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div/div/div[3]/div[3]/a/div')
+    clickElement(
+        driver1,
+        wait1,
+        '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div/div/div[3]/div[3]/a/div'
+    )
 
     send_keysElement(
+        wait1,
         '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[4]/label/div/div[2]/div/input', 
         os.getenv('EMAIL')
     )
-    clickElement('//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/button[2]/div')
+    clickElement(
+        driver1,
+        wait1,
+        '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/button[2]/div'
+    )
 
     send_keysElement(
+        wait1,
         '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input',
         os.getenv('XUSERNAME')
     )
-    clickElement('//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/button/div')
+    clickElement(
+        driver1,
+        wait1,
+        '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/button/div'
+    )
 
     send_keysElement(
+        wait1,
         '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input',
         os.getenv('PASSWORD')
     )
-    clickElement('//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/button/div')
+    clickElement(
+        driver1,
+        wait1,
+        '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/button/div'
+    )
     
     #input("Login... \nPress enter to continue...")
 
     for i in range(5):
-        text = locateElement(f'/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[5]/section/div/div/div[{i}]/div/div/article/div/div/div[2]/div[2]/div[2]')
+        text = locateElement(
+            wait1,
+            f'/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[5]/section/div/div/div[{i}]/div/div/article/div/div/div[2]/div[2]/div[2]'
+        )
         print(text)
 
+def signInOpenai():
+    """
+        Do to OpenAI having better anti-botting protection,
+        you have to sign in manually.
+    """
+    driver2.get('https://www.google.com/')
+    print("Please Sign into OpenAI ChatGPT manually...")
+    input("Press Enter to proceed:")
+    # test if signed in
+    # send inital query
 
+def run():
+    #signInTwitter()
+    signInOpenai()
 
 if __name__ == '__main__':
     run()
-    input()
+
+
+
+    input("quit:")
